77b9bfc0449113c7dec36e16138dc236be6104fd
thepatriotscommandxs <108895855+thepatriotscommandxs@users.noreply.github.com>
2022-12-08 06:48:14 -0600

    Update splitter.js

--- splitter.js
+++ splitter.js
@@ -1,19 +1,95 @@
-const fs = require('fs');
-const https = require('https');
+function initRun() {
+  fetch('https://raw.githubusercontent.com/thepatriotscommandxs/thepatriotscommandxs.github.io/main/data/20th-century-african-leaders.txt')
+      .then(response => response.text())
+      .then(text => {
+      
+          let full = text.split("']\n");
+          const rand = Math.abs(Math.round(Math.random() * full.length-2));
 
-// Get the text file from the GitHub repository
-https.get('https://raw.githubusercontent.com/thepatriotscommandxs/thepatriotscommandxs.github.io/main/data/20th-century-african-leaders.txt', (res) => {
-  let data = '';
-  // Read the data in chunks
-  res.on('data', (chunk) => {
-    data += chunk;
-  });
-  // Once the file is read, index the sentences
-  res.on('end', () => {
-    const sentences = data.split("']\n");
-    const rand = Math.round(Math.random() * sentences.length);
-    Document.getElementById("text").innerText=sentences[rand];
-    console.log(`${sentences[rand]}`);
+       
+
+
+          full.join('').split('');
+
+          function checkStr() {
+          if(full[rand]=="undefined") {
+            const rand = Math.abs(Math.round(Math.random() * full.length-2));
+            rand = abs(rand);
+
+            checkStr()
+            
+            }
+            
+            else {       
+
+
+
+            let AnsHint = full[rand].split("', '");
+            console.log("", AnsHint)
+            let Ans = AnsHint[0].replace("['","");
+
+            let ArrayAns = Ans.split(" ");
+            console.log("", ArrayAns)
+
+            let Hint = AnsHint[1].replace(/;/gi,"-")
+            console.log("", Hint)
+
+            let sentences = Hint.split(". ")
+              
+              
+              CurrentHint += sentences[count]+ "."
+  
+  
+              //document.getElementById('DUMMY').innerHTML = count;
+              console.log("", count)
+           //document.getElementById('Prompt').innerHTML = ArrayAns+" ====>> "+CurrentHint;
+           console.log("", CurrentHint)
+
+              
+              
+              
+              
+              
+              
+            }
+      }
+          
+          checkStr()
+          
+          
+          
+  
+          
+          
+          
+          
+      });
+  
+  
+  }
+  
+  let CurrentHint = '';
+  
+  let count = 0;
+  
+  
+  
+  function addCount(){
+    count+=1
+  
+  
+    document.getElementById('DUMMY').innerHTML = count;
     
-  });
-});
+    
+    CurrentHint += sentence[count] + "."
+    
+    
+    
+    document.getElementById('Prompt').innerHTML = ArrayAns+"   "+CurrentHint;
+  
+  
+  }
+  
+      
+  initRun()
+      
