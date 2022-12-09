5de90e73bf237124e23d2b6a78611789b85ee8ed
thepatriotscommandxs <108895855+thepatriotscommandxs@users.noreply.github.com>
2022-12-08 20:41:52 -0600

    Fixed, working on answer val

--- splitter.js
+++ splitter.js
@@ -1,35 +1,12 @@
-function getTitles() {
-  fetch("https://github.com/thepatriotscommandxs/thepatriotscommandxs.github.io/tree/main/data")
-      .then((result) => { return result.text(); })
-      .then((text) => { 
-      text = text.replaceAll ("\r", "").replaceAll ("\n", "");
+fetch('https://raw.githubusercontent.com/thepatriotscommandxs/thepatriotscommandxs.github.io/main/total.txt')
+
+.then(response => response.text())
+  .then(text => {
+      let full = text.split("']\r\n");
+
+      const rand = Math.round(Math.random() * full.length);
       
-      full = text.split('-open Link--primary" title="');
-      //document.getElementById('Logger').innerHTML = full
-
-        
-      full.splice(0,1)
-        
-        for(i = full.length-1;i--;) {
-          const reg = /data-turbo-frame="repo-content-turbo-frame"+.*/gi
-          if(full[i]!=undefined) {
-          
-          full[i] = full[i].replace(reg,"")
-
-          full[i] = full[i].replace('"',"")
-          
-          }
-        }  
-        const results = full.filter(element => {
-          if (Object.keys(element).length !== 0) {
-            return true;
-          }
-        
-          return false;
-        });
-
-        const rand2 = Math.round(Math.random() * full.length);
-        console.log(results)
-        //window.webSite = full[rand2]
-      });}
-      getTitles()
+      console.log(full)
+    
+    
+    })
